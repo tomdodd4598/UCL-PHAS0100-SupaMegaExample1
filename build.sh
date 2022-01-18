d8edf129fed0eb1b0187a83b55887f785a650871
Tom Dodd <joedodd35@gmail.com>
2022-01-18 04:31:26 +0000

    Modified shell script to perform Make tests

--- build.sh
+++ build.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-mkdir -p build
-cd build
-mkdir -p release
-mkdir -p debug
-
-cd release
-cmake -DCMAKE_BUILD_TYPE=Release ../..
-make
-
-cd ../debug
-cmake -DCMAKE_BUILD_TYPE=Debug ../..
-make
-
-cd ../..
+if [[ $# -eq 0 ]]; then
+    mkdir -p build
+    ./build.sh release Release &
+    ./build.sh debug Debug &
+    wait
+    exit 0
+else
+    mkdir -p build/"$1"
+    cd build/"$1"
+    cmake -DCMAKE_BUILD_TYPE="$2" ../..
+    make
+    make test
+fi
